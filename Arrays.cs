f95b5a9275e38f098e5138b500e0e99b91707439
annkostet <annakostetska30@gmail.com>
2021-11-17 16:44:26 +0200

    Add project files.

---
+++ Arrays.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anya_Arrays
+{
+    class Arrays
+    {
+        public static int GetMinOfArray(int[] array)
+        {
+            int minValue = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < minValue)
+                {
+                    minValue = array[i];
+                }
+            }
+
+            return minValue;
+        }
+
+        public static int GetMaxOfArray(int[] array)
+        {
+            int maxValue = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > maxValue)
+                {
+                    maxValue = array[i];
+                }
+            }
+
+            return maxValue;
+        }
+
+        public static int GetIndexOfMinValueOfArray(int[] array)
+        {
+            //Array.Sort(array);
+            int minValue = array[0];
+            int minIndex = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < minValue)
+                {
+                    minValue = array[i];
+                    minIndex = i;
+                }
+            }
+
+            return minIndex;
+        }
+
+        public static int GetIndexOfMaxValueOfArray(int[] array)
+        {
+            //Array.Sort(array);
+            int maxValue = array[0];
+            int maxIndex = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > maxValue)
+                {
+                    maxValue = array[i];
+                    maxIndex = i;
+                }
+            }
+
+            return maxIndex;
+        }
+    }
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Anya_Arrays;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            int[] array = new int[10];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(100);
+            }
+            Console.WriteLine(Arrays.GetIndexOfMaxValueOfArray(array));
+
+            //Array.Sort(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine($"{i} {Arrays.GetMinOfArray(array)}");
+            //}
+        }
+    }
+}
